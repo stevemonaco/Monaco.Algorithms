673658ba470123e0b6e8f78284458fe8eaf19b99
Stephen Monaco <stevemonaco@hotmail.com>
2018-11-22 01:56:56 -0500

    Initial commit. Implemented generic RandomShuffle and Swap.

---
+++ Monaco.Algorithms/Extensions/ListExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monaco.Algorithms.Extensions
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Randomly shuffles the list using a Fisher-Yates shuffle
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="random">A seeded instance of Random</param>
+        public static void RandomShuffle<T>(this IList<T> list, Random random)
+        {
+            for(int n = list.Count - 1; n > 0; n--)
+            {
+                int swapIndex = random.Next(0, n);
+                var temp = list[n];
+                list[n] = list[swapIndex];
+                list[swapIndex] = temp;
+            }
+        }
+
+        public static void Swap<T>(this IList<T> list, int firstIndex, int secondIndex)
+        {
+            var temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+        }
+    }
+}

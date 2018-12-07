f08308350621ac70333e814b79ccdb472664234a
Stephen Monaco <stevemonaco@hotmail.com>
2018-12-07 06:18:27 -0500

    Initial commit

---
+++ Monaco.Algorithms.UnitTests/Checksum/InternetChecksumTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Monaco.Algorithms.Checksum;
+using NUnit.Framework;
+
+namespace Monaco.Algorithms.UnitTests.Checksum
+{
+    [TestFixture]
+    public class InternetChecksumTests
+    {
+        [TestCase(new byte[] { 0xFF }, (ushort)0x00FF)]
+        [TestCase(new byte[] { 0x00, 0x01, 0xF2, 0x03, 0xF4, 0xF5, 0xF6, 0xF7 }, (ushort)0x220D) ]
+        [TestCase(new byte[] { 0xe3, 0x4f, 0x23, 0x96, 0x44, 0x27, 0x99, 0xf3 }, (ushort)0x1AFF)]
+        public void MakeChecksum_ValidMessage_MakesCorrectly(byte[] message, ushort expectedChecksum)
+        {
+            var actualChecksum = InternetChecksum.MakeChecksum(message);
+
+            Assert.AreEqual(expectedChecksum, actualChecksum, $"Expected {expectedChecksum:X} Actual {actualChecksum:X}");
+        }
+
+        [TestCase(new byte[] { })]
+        public void MakeChecksum_EmptyMessage_ThrowsArgumentException(byte[] message)
+        {
+            Assert.Throws<ArgumentException>(() => InternetChecksum.MakeChecksum(message));
+        }
+
+        [TestCase(null)]
+        public void MakeChecksum_NullMessage_ThrowsNullReferenceException(byte[] message)
+        {
+            Assert.Throws<NullReferenceException>(() => InternetChecksum.MakeChecksum(message));
+        }
+    }
+}
---
+++ Monaco.Algorithms/Checksum/InternetChecksum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Monaco.Algorithms.Checksum
+{
+    /// <summary>
+    /// Implementation of RFC1071's Internet Checksum which is a 16bit one's complement checksum.
+    /// </summary>
+    public class InternetChecksum
+    {
+        /// <summary>
+        /// Makes an Internet Checksum from the supplied message
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns>The checksum</returns>
+        /// <exception cref="NullReferenceException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static ushort MakeChecksum(byte[] message)
+        {
+            if (message is null)
+                throw new NullReferenceException();
+
+            if (message.Length == 0)
+                throw new ArgumentException($"{nameof(message)} was empty");
+
+            int sum = 0;
+
+            for(int i = 0; i < message.Length - 1; i += 2)
+            {
+                ushort pair = (ushort) ((message[i] << 8) + message[i + 1]);
+                sum += pair;
+            }
+
+            if (message.Length % 2 != 0)
+                sum += (message[message.Length - 1] << 8);
+
+            while ((sum >> 16) > 0)
+                sum = (sum & 0xFFFF) + (sum >> 16);
+
+            return (ushort)~sum;
+        }
+    }
+}

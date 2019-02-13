d958a28c3a25af8781c2bca60c8f77d09e6b48ee
Stephen Monaco <stevemonaco@hotmail.com>
2019-02-13 04:54:07 -0500

    Initial implementation

---
+++ Monaco.Algorithms.UnitTests/Searching/LinearStringMatcherTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Monaco.Algorithms.Searching;
+using NUnit.Framework;
+
+namespace Monaco.Algorithms.UnitTests.Searching
+{
+    [TestFixture]
+    class LinearStringMatcherTests
+    {
+        [TestCaseSource(typeof(StringMatcherTestCases), "FindFirstTestCases")]
+        public void FindFirst_ReturnsExpected(string text, string pattern, int startIndex, int expected)
+        {
+            var matcher = new LinearStringMatcher();
+            int actual = matcher.FindFirst(text, pattern, startIndex);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCaseSource(typeof(StringMatcherTestCases), "FindAllTestCases")]
+        public void FindAll_ReturnsExpected(string text, string pattern, int startIndex, IEnumerable<int> expected)
+        {
+            var matcher = new LinearStringMatcher();
+            var actual = matcher.FindAll(text, pattern, startIndex).ToList();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Monaco.Algorithms/Searching/LinearStringMatcher.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monaco.Algorithms.Searching
+{
+    /// <summary>
+    /// Implementation of a naive, linear string matching algorithm
+    /// O(m*n) runtime complexity
+    /// </summary>
+    public class LinearStringMatcher : IStringMatcher
+    {
+        /// <summary>
+        /// Finds the first occurance of the pattern in the text
+        /// </summary>
+        /// <returns>The index if found or -1 if not found</returns>
+        public int FindFirst(string text, string pattern, int startIndex = 0)
+        {
+            if (text is null || pattern is null)
+                throw new NullReferenceException();
+
+            for (int i = startIndex; i < text.Length - pattern.Length + 1; i++)
+            {
+                bool match = true;
+                for (int j = 0; j < pattern.Length; j++)
+                {
+                    if (text[i+j] != pattern[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                    return i;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Finds all occurances of the pattern in the text
+        /// </summary>
+        /// <returns>An enumerable of indices or an empty enumerable if not found</returns>
+        public IEnumerable<int> FindAll(string text, string pattern, int startIndex = 0)
+        {
+            if (text is null || pattern is null)
+                throw new NullReferenceException();
+
+            for (int i = startIndex; i < text.Length - pattern.Length + 1; i++)
+            {
+                bool match = true;
+                for (int j = 0; j < pattern.Length; j++)
+                {
+                    if (text[i+j] != pattern[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                    yield return i;
+            }
+        }
+    }
+}
